ed1ea9d55457d75ef76291e4c98a120013c43cf3
giresse21 <giresseayefou@gmail.com>
2026-02-22 15:05:42 +0100

    key change

--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -1,7 +1,10 @@
 module JsonWebToken
   extend self
 
-  SECRET_KEY = Rails.application.credentials.secret_key_base
+  # Utiliser une clé secrète qui fonctionne en CI
+  SECRET_KEY = Rails.application.secret_key_base ||
+               Rails.application.credentials.secret_key_base ||
+               "fallback_secret_key_for_test"
 
   def encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
